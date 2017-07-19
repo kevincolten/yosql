dae5cba58ee1bb71a5bb88cb33b7f8018573abad
Kevin Colten <kevincolten@gmail.com>
2017-07-19 00:47:33 -0500

    still need to combine queries

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,7 @@ function loadDatabase(uri, filename, options, callback) {
     var database = createDatabase(filename);
     _mongodb.MongoClient.connect(uri, function (err, mongodb) {
       if (error) return callback(error);
-      // console.log("Connected correctly to server");
+      // console.log('Connected correctly to server');
       mongodb.collections().then(function (collections) {
         database.serialize(function () {
           createTables(database, collections, {}, options, function () {
@@ -47,7 +47,7 @@ function createTables(database, collections, schema, options, callback) {
     return collections[idx].find().toArray(function (error, documents) {
       if (error) console.error(error);
       if (!options.ignore.includes(tableName)) {
-        createTable(database, tableName, documents, schema, function () {
+        createTable(tableName, documents, schema, function () {
           return createTables(database, collections, schema, options, callback, ++idx);
         });
       } else {
@@ -58,116 +58,112 @@ function createTables(database, collections, schema, options, callback) {
   return callback();
 }
 
-function createTable(database, tableName, documents, schema, callback, columns) {
-  var idx = arguments.length > 6 && arguments[6] !== undefined ? arguments[6] : 0;
+function createTable(tableName, documents, schema, callback, columns) {
+  var idx = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : 0;
 
   if (!schema[tableName]) {
-    schema[tableName] = { columns: {}, length: 0 };
-    // console.log(`CREATE TABLE "${tableName}" ("yosql_id" INTEGER PRIMARY KEY UNIQUE);`);
-    schema[tableName]['yosql_id'] = 'INTEGER PRIMARY KEY UNIQUE';
-    return database.run('CREATE TABLE "' + tableName + '" ("yosql_id" INTEGER PRIMARY KEY UNIQUE);', function () {
-      return createTable(database, tableName, documents, schema, callback, columns, idx);
-    });
+    schema[tableName] = { columns: {}, length: 0, query: [] };
+    // console.log(`CREATE TABLE '${tableName}' ('yosql_id' INTEGER PRIMARY KEY UNIQUE);`);
+    schema[tableName]['columns']['yosql_id'] = 'INTEGER PRIMARY KEY UNIQUE';
+    schema[tableName].query.push('CREATE TABLE \'' + tableName + '\' (\'yosql_id\' INTEGER PRIMARY KEY UNIQUE)');
+    return createTable(tableName, documents, schema, callback, columns, idx);
   } else if (columns && idx < columns.length) {
-    return addColumn(tableName, columns[idx], function () {
-      return createTable(database, tableName, documents, schema, callback, columns, ++idx);
+    return addColumn(tableName, columns[idx], schema, function () {
+      return createTable(tableName, documents, schema, callback, columns, ++idx);
     });
   } else if (!columns && documents.length) {
     columns = Object.keys(documents[0]).filter(function (key) {
       return parseValue(_typeof(documents[0][key])) !== 'object';
     });
-
-    return addColumn(tableName, columns[idx], function () {
-      return createTable(database, tableName, documents, schema, callback, columns, ++idx);
+    return addColumn(tableName, columns[idx], schema, function () {
+      return createTable(tableName, documents, schema, callback, columns, ++idx);
     });
   }
-
   columns = columns || [];
-  return insertRows(tableName, columns, documents, callback);
+  return insertRows(tableName, columns, documents, schema, callback);
+}
 
-  function addColumn(tableName, column, callback) {
-    if (schema[tableName]['columns'][column]) return callback();
-    schema[tableName]['columns'][column] = 'TEXT';
-    // console.log(`ALTER TABLE "${tableName}" ADD COLUMN "${column}" TEXT;`);
-    return database.run('ALTER TABLE "' + tableName + '" ADD COLUMN "' + column + '" TEXT;', callback);
-  }
-
-  function insertRow(tableName, columns, document, callback) {
-    var newColumn = Object.keys(document).find(function (key) {
-      return !columns.includes(key);
-    });
-
-    if (newColumn) {
-      document[newColumn] = parseValue(document[newColumn]);
-      if (Array.isArray(document[newColumn])) {
-        if (document[newColumn].length) {
-          if (typeof parseValue(document[newColumn][0]) === 'string') {
-            document[newColumn] = document[newColumn].map(function (item) {
-              var obj = {};
-              obj[newColumn] = parseValue(item);
-              return obj;
-            });
-          }
-          document[newColumn].map(function (obj) {
-            return obj || {};
-          }).forEach(function (obj) {
-            return obj[tableName + '_yosql_id'] = document['yosql_id'];
-          });
-          // console.log(`${tableName}_${newColumn}`);
-          return createTable(database, tableName + '_' + newColumn, document[newColumn], schema, function () {
-            delete document[newColumn];
-            return insertRow(tableName, columns, document, callback);
-          });
-        } else {
-          delete document[newColumn];
-          return insertRow(tableName, columns, document, callback);
-        }
-      }
-      columns.push(newColumn);
-      return addColumn(tableName, newColumn, function () {
-        insertRow(tableName, columns, document, callback);
-      });
-    }
-
-    var values = [];
-    var cleanColumns = [];
-    columns.forEach(function (column) {
-      cleanColumns.push('"' + column + '"');
-      values.push('"' + parseValue(document[column]) + '"');
-    });
-    return database.run('INSERT INTO "' + tableName + '" (' + cleanColumns.join(', ') + ') VALUES (' + values.join(', ') + ');', callback);
-  }
-
-  function insertRows(tableName, columns, documents, callback) {
-    var idx = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 0;
-
-    if (idx < documents.length) {
-      documents[idx]['yosql_id'] = ++schema[tableName].length;
-      return insertRow(tableName, columns, documents[idx], function () {
-        return insertRows(tableName, columns, documents, callback, ++idx);
-      });
-    }
-    return callback();
-  }
-
-  function parseValue(value) {
-    if ((typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object') {
-      if (value instanceof Date) {
-        return value.toISOString();
-      } else if (Array.isArray(value)) {
-        return value;
-      } else if (_bson.ObjectId.isValid(value)) {
-        return value.toString();
-      } else if (value === null) {
-        return '';
-      } else {
-        return [value];
-      }
-    } else if (value === undefined) {
+function parseValue(value) {
+  if ((typeof value === 'undefined' ? 'undefined' : _typeof(value)) === 'object') {
+    if (value instanceof Date) {
+      return value.toISOString();
+    } else if (Array.isArray(value)) {
+      return value;
+    } else if (_bson.ObjectId.isValid(value)) {
+      return value.toString();
+    } else if (value === null) {
       return '';
+    } else {
+      return [value];
     }
-    return value;
+  } else if (value === undefined) {
+    return '';
+  } else if (typeof value === 'string') {
+    return value.replace(/\'/g, "''");
   }
+  return value;
+}
+
+function insertRows(tableName, columns, documents, schema, callback) {
+  documents.forEach(function (document) {
+    document.yosql_id = ++schema[tableName].length;
+    insertRow(tableName, columns, document, schema);
+  });
+  return callback();
+}
+
+function insertRow(tableName, columns, document, schema) {
+  var newColumn = Object.keys(document).find(function (key) {
+    return !columns.includes(key);
+  });
+
+  if (newColumn) {
+    document[newColumn] = parseValue(document[newColumn]);
+    if (Array.isArray(document[newColumn])) {
+      if (document[newColumn].length) {
+        if (typeof parseValue(document[newColumn][0]) === 'string') {
+          document[newColumn] = document[newColumn].map(function (item) {
+            var obj = {};
+            obj[newColumn] = parseValue(item);
+            return obj;
+          });
+        }
+        document[newColumn].map(function (obj) {
+          return obj || {};
+        }).forEach(function (obj) {
+          return obj[tableName + '_yosql_id'] = document['yosql_id'];
+        });
+        // console.log(`${tableName}_${newColumn}`);
+        return createTable(tableName + '_' + newColumn, document[newColumn], schema, function () {
+          delete document[newColumn];
+          return insertRow(tableName, columns, document, schema);
+        });
+      } else {
+        delete document[newColumn];
+        return insertRow(tableName, columns, document, schema);
+      }
+    }
+    columns.push(newColumn);
+    return addColumn(tableName, newColumn, schema, function () {
+      insertRow(tableName, columns, document, schema);
+    });
+  }
+
+  var values = [];
+  var cleanColumns = [];
+  columns.forEach(function (column) {
+    cleanColumns.push('\'' + column + '\'');
+    values.push('\'' + parseValue(document[column]) + '\'');
+  });
+  return schema[tableName].query.push('INSERT INTO \'' + tableName + '\' (' + cleanColumns.join(', ') + ') VALUES (' + values.join(', ') + ')');
+}
+
+function addColumn(tableName, column, schema, callback) {
+  if (schema[tableName]['columns'][column]) return callback();
+  schema[tableName]['columns'][column] = 'TEXT';
+  // console.log(`ALTER TABLE '${tableName}' ADD COLUMN '${column}' TEXT;`);
+  schema[tableName].query.push('ALTER TABLE \'' + tableName + '\' ADD COLUMN \'' + column + '\' TEXT');
+  return callback();
 }
 
 function runQuery(query, filename, callback) {
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function loadDatabase(uri, filename, options, callback) {
     const database = createDatabase(filename);
     MongoClient.connect(uri, function (err, mongodb) {
       if (error) return callback(error);
-      // console.log("Connected correctly to server");
+      // console.log('Connected correctly to server');
       mongodb.collections().then(collections => {
         database.serialize(() => {
           createTables(database, collections, {}, options, () => {
@@ -35,7 +35,7 @@ function createTables(database, collections, schema, options, callback, idx = 0)
     return collections[idx].find().toArray((error, documents) => {
       if (error) console.error(error);
       if (!options.ignore.includes(tableName)) {
-        createTable(database, tableName, documents, schema, () => {
+        createTable(tableName, documents, schema, () => {
           return createTables(database, collections, schema, options, callback, ++idx);
         });
       } else {
@@ -47,112 +47,111 @@ function createTables(database, collections, schema, options, callback, idx = 0)
   return callback();
 }
 
-function createTable(database, tableName, documents, schema, callback, columns, idx = 0) {
+function createTable(tableName, documents, schema, callback, columns, idx = 0) {
   if (!schema[tableName]) {
-    schema[tableName] = { columns: {}, length: 0 };
-    // console.log(`CREATE TABLE "${tableName}" ("yosql_id" INTEGER PRIMARY KEY UNIQUE);`);
+    schema[tableName] = { columns: {}, length: 0, query: [] };
+    // console.log(`CREATE TABLE '${tableName}' ('yosql_id' INTEGER PRIMARY KEY UNIQUE);`);
     schema[tableName]['columns']['yosql_id'] = 'INTEGER PRIMARY KEY UNIQUE';
-    return database.run(`CREATE TABLE "${tableName}" ("yosql_id" INTEGER PRIMARY KEY UNIQUE);`, () => {
-      return createTable(database, tableName, documents, schema, callback, columns, idx);
-    });
+    schema[tableName].query.push(`CREATE TABLE '${tableName}' ('yosql_id' INTEGER PRIMARY KEY UNIQUE)`);
+    return createTable(tableName, documents, schema, callback, columns, idx);
   } else if (columns && idx < columns.length) {
-    return addColumn(tableName, columns[idx], () => {
-      return createTable(database, tableName, documents, schema, callback, columns, ++idx);
+    return addColumn(tableName, columns[idx], schema, () => {
+      return createTable(tableName, documents, schema, callback, columns, ++idx);
     });
   } else if (!columns && documents.length) {
     columns = Object.keys(documents[0]).filter(key => {
       return parseValue(typeof documents[0][key]) !== 'object';
     });
-
-    return addColumn(tableName, columns[idx], () => {
-      return createTable(database, tableName, documents, schema, callback, columns, ++idx);
+    return addColumn(tableName, columns[idx], schema, () => {
+      return createTable(tableName, documents, schema, callback, columns, ++idx);
     });
   }
-
   columns = columns || [];
-  return insertRows(tableName, columns, documents, callback);
+  return insertRows(tableName, columns, documents, schema, callback);
+}
 
-  function addColumn(tableName, column, callback) {
-    if (schema[tableName]['columns'][column]) return callback();
-    schema[tableName]['columns'][column] = 'TEXT';
-    // console.log(`ALTER TABLE "${tableName}" ADD COLUMN "${column}" TEXT;`);
-    return database.run(`ALTER TABLE "${tableName}" ADD COLUMN "${column}" TEXT;`, callback);
-  }
-
-  function insertRow(tableName, columns, document, callback) {
-    const newColumn = Object.keys(document).find(key => {
-      return !columns.includes(key);
-    });
-
-    if (newColumn) {
-      document[newColumn] = parseValue(document[newColumn]);
-      if (Array.isArray(document[newColumn])) {
-        if (document[newColumn].length) {
-          if (typeof parseValue(document[newColumn][0]) === 'string') {
-            document[newColumn] = document[newColumn].map(item => {
-              const obj = {};
-              obj[newColumn] = parseValue(item);
-              return obj;
-            });
-          }
-          document[newColumn].map((obj) => {
-            return obj || {};
-          }).forEach(obj => {
-            return obj[`${tableName}_yosql_id`] = document['yosql_id'];
-          });
-          // console.log(`${tableName}_${newColumn}`);
-          return createTable(database, `${tableName}_${newColumn}`, document[newColumn], schema, () => {
-            delete document[newColumn];
-            return insertRow(tableName, columns, document, callback);
-          });
-        } else {
-          delete document[newColumn];
-          return insertRow(tableName, columns, document, callback);
-        }
-      }
-      columns.push(newColumn);
-      return addColumn(tableName, newColumn, () => {
-        insertRow(tableName, columns, document, callback);
-      });
-    }
-
-    const values = [];
-    const cleanColumns = [];
-    columns.forEach(column => {
-      cleanColumns.push(`"${column}"`);
-      values.push(`"${parseValue(document[column])}"`);
-    });
-    return database.run(`INSERT INTO "${tableName}" (${cleanColumns.join(', ')}) VALUES (${values.join(', ')});`, callback);
-  }
-
-  function insertRows(tableName, columns, documents, callback, idx = 0) {
-    if (idx < documents.length) {
-      documents[idx]['yosql_id'] = ++schema[tableName].length;
-      return insertRow(tableName, columns, documents[idx], () => {
-        return insertRows(tableName, columns, documents, callback, ++idx);
-      });
-    }
-    return callback();
-  }
-
-  function parseValue(value) {
-    if (typeof value === 'object') {
-      if (value instanceof Date) {
-        return value.toISOString();
-      } else if (Array.isArray(value)) {
-        return value;
-      } else if (ObjectId.isValid(value)) {
-        return value.toString();
-      } else if (value === null) {
-        return '';
-      } else {
-        return [value];
-      }
-    } else if (value === undefined) {
+function parseValue(value) {
+  if (typeof value === 'object') {
+    if (value instanceof Date) {
+      return value.toISOString();
+    } else if (Array.isArray(value)) {
+      return value;
+    } else if (ObjectId.isValid(value)) {
+      return value.toString();
+    } else if (value === null) {
       return '';
+    } else {
+      return [value];
     }
-    return value;
+  } else if (value === undefined) {
+    return '';
+  } else if (typeof value === 'string') {
+    return value.replace(/\'/g,"''")
   }
+  return value;
+}
+
+function insertRows(tableName, columns, documents, schema, callback) {
+  documents.forEach(document => {
+    document.yosql_id = ++schema[tableName].length;
+    insertRow(tableName, columns, document, schema);
+  });
+  return callback();
+}
+
+function insertRow(tableName, columns, document, schema) {
+  const newColumn = Object.keys(document).find(key => {
+    return !columns.includes(key);
+  });
+
+  if (newColumn) {
+    document[newColumn] = parseValue(document[newColumn]);
+    if (Array.isArray(document[newColumn])) {
+      if (document[newColumn].length) {
+        if (typeof parseValue(document[newColumn][0]) === 'string') {
+          document[newColumn] = document[newColumn].map(item => {
+            const obj = {};
+            obj[newColumn] = parseValue(item);
+            return obj;
+          });
+        }
+        document[newColumn].map((obj) => {
+          return obj || {};
+        }).forEach(obj => {
+          return obj[`${tableName}_yosql_id`] = document['yosql_id'];
+        });
+        // console.log(`${tableName}_${newColumn}`);
+        return createTable(`${tableName}_${newColumn}`, document[newColumn], schema, () => {
+          delete document[newColumn];
+          return insertRow(tableName, columns, document, schema);
+        });
+      } else {
+        delete document[newColumn];
+        return insertRow(tableName, columns, document, schema);
+      }
+    }
+    columns.push(newColumn);
+    return addColumn(tableName, newColumn, schema, () => {
+      insertRow(tableName, columns, document, schema);
+    });
+  }
+
+  const values = [];
+  const cleanColumns = [];
+  columns.forEach(column => {
+    cleanColumns.push(`'${column}'`);
+    values.push(`'${parseValue(document[column])}'`);
+  });
+  return schema[tableName].query.push(`INSERT INTO '${tableName}' (${cleanColumns.join(', ')}) VALUES (${values.join(', ')})`);
+}
+
+
+function addColumn(tableName, column, schema, callback) {
+  if (schema[tableName]['columns'][column]) return callback();
+  schema[tableName]['columns'][column] = 'TEXT';
+  // console.log(`ALTER TABLE '${tableName}' ADD COLUMN '${column}' TEXT;`);
+  schema[tableName].query.push(`ALTER TABLE '${tableName}' ADD COLUMN '${column}' TEXT`);
+  return callback();
 }
 
 function runQuery(query, filename, callback) {
